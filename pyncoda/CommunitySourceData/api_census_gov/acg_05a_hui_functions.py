70ff6dd35dada568f523b1bc166465d8f60f0234
Nathanael Rosenheim <nrosenheim@arch.tamu.edu>
2022-11-30 16:23:09 -0600

    adjusting workflow for prec
    setting up workflow to add person record file creation

--- pyncoda/CommunitySourceData/api_census_gov/acg_05a_hui_functions.py
+++ pyncoda/CommunitySourceData/api_census_gov/acg_05a_hui_functions.py
@@ -32,7 +32,10 @@ from pyncoda.CommunitySourceData.api_census_gov.acg_00e_incore_huiv2 \
 from pyncoda.CommunitySourceData.api_census_gov.acg_01a_BaseInventory import BaseInventory
 from pyncoda.CommunitySourceData.api_census_gov.acg_02a_add_categorical_char \
      import add_new_char_by_random_merge_2dfs
-
+from pyncoda.CommunitySourceData.api_census_gov.acg_02b_incomefunctions \
+     import *
+from pyncoda.CommunitySourceData.api_census_gov.acg_02c_polishdf \
+     import *
 
 class hui_workflow_functions():
     """
@@ -232,234 +235,7 @@ class hui_workflow_functions():
             print("File saved:",savefile)
         
         return hui_df
-    # Code for adding random income
 
-    # Dictionary with round options
-    @staticmethod
-    def make_incomegroup_dict():
-        ## Add min and max income values
-        incomegroup_dict = {1: {'minincome': 0, 'maxincome': 9999},
-        2: {'minincome': 10000, 'maxincome': 14999},
-        3: {'minincome': 15000, 'maxincome': 19999},
-        4: {'minincome': 20000, 'maxincome': 24999},
-        5: {'minincome': 25000, 'maxincome': 29999},
-        6: {'minincome': 30000, 'maxincome': 34999},
-        7: {'minincome': 35000, 'maxincome': 39999},
-        8: {'minincome': 40000, 'maxincome': 44999},
-        9: {'minincome': 45000, 'maxincome': 49999},
-        10: {'minincome': 50000, 'maxincome': 59999},
-        11: {'minincome': 60000, 'maxincome': 74999},
-        12: {'minincome': 75000, 'maxincome': 99999},
-        13: {'minincome': 100000, 'maxincome': 124999},
-        14: {'minincome': 125000, 'maxincome': 149999},
-        15: {'minincome': 150000, 'maxincome': 199999},
-        16: {'minincome': 200000, 'maxincome': 250000},
-        -999 : {'minincome': 0, 'maxincome': 250000, 'note' : 
-        'Category -999 = income group not set. Assume income could be anything from 0 to 250000.'},
-        0 : {'minincome': 0, 'maxincome': 1, 'note' : 
-        'Category 0 = observation has no income, but the random income process requires'+\
-            'a value to work for all observations. Remove ranincome after set.'+\
-                'This category applies to vacant housing units and group quarters.'} }
-
-        return incomegroup_dict
-
-    @staticmethod
-    def add_minmaxincome(input_df,incomegroup_dict):
-        #condition = (df['incomegroup'].notnull())
-        output_df = input_df.copy()
-        output_df['minincome'] = output_df.\
-            apply(lambda x: incomegroup_dict[x['incomegroup']]['minincome'], axis=1)
-        output_df['maxincome'] = output_df.\
-            apply(lambda x: incomegroup_dict[x['incomegroup']]['maxincome'], axis=1)
-
-        return output_df
-
-    @staticmethod
-    def remove_cat0_randincome(input_df):
-        """
-        For category 0 need to replace values with missing.
-        Without this step the descriptive statistics (median, mean)
-        will be skewed. 
-        Category 0 represents observations where the 
-        characteristic is not applicable.
-        For example - vacant housing units do not have a household income.
-        """
-
-        output_df = input_df.copy()
-        condition = (output_df['incomegroup'] == 0)
-        output_df.loc[condition, 'minincome'] = np.nan
-        output_df.loc[condition, 'maxincome'] = np.nan
-        output_df.loc[condition, 'randincomeB19101'] = np.nan
-
-        return output_df
-
-    def add_randincome(self, df, seed):
-        random_generator = np.random.RandomState(seed)
-
-        output_df = df.copy()
-        #Make sure income group has 0 category
-        output_df['incomegroup'] = \
-        output_df['incomegroup'].fillna(value=0)
-        output_df['incomegroup'].describe()
-
-        # read in incomegroup dictionary
-        incomegroup_dict = self.make_incomegroup_dict()
-        # Add min max values
-        output_df = self.add_minmaxincome(output_df,incomegroup_dict)
-
-        # Add random income value
-        output_df['randincomeB19101'] = output_df.apply(lambda x: \
-            random_generator.randint(x['minincome'],x['maxincome']), axis=1)
-        
-        # remove income from income category 0
-        output_df = self.remove_cat0_randincome(output_df)
-
-        # add 5 household income groups
-        output_df = self.add_hhinc_groups(output_df)
-
-        return output_df
-
-    def add_poverty(self,input_df):
-        """
-        Add poverty based on US Census Poverty Thresholds
-        https://www.census.gov/topics/income-poverty/poverty/guidance/poverty-measures.html
-        https://www2.census.gov/programs-surveys/cps/tables/time-series/historical-poverty-thresholds/thresh12.xls
-
-        Use weighted average threshold by household size
-        """
-
-        output_df = input_df.copy()
-
-        poverty_by_numprec_dict = {1: 11720,
-                    2:  14937,
-                    3: 18284,
-                    4: 23492,
-                    5: 27827,
-                    6: 31471,
-                    7: 35743}
-
-        for numprec in poverty_by_numprec_dict:
-            randincome_less_than    = (output_df['randincomeB19101'] < \
-                poverty_by_numprec_dict[numprec])
-            numprec_equals = (output_df['numprec'] == numprec)
-            conditions = randincome_less_than & numprec_equals
-            output_df.loc[conditions,'poverty'] = 1
-
-            randincome_greater_than    = (output_df['randincomeB19101'] >= \
-                poverty_by_numprec_dict[numprec])
-            conditions = randincome_greater_than & numprec_equals
-            output_df.loc[conditions,'poverty'] = 0
-
-        # Add 0 hhinc - for no income group
-        randincome_missing =  (output_df['randincomeB19101'].isnull())
-        is_gqtype  = (output_df['gqtype'] > 0)
-        is_vacant  = (output_df['vacancy'] > 0)
-        conditions = randincome_missing | is_gqtype | is_vacant
-        output_df.loc[conditions,'poverty'] = np.nan
-
-        return output_df
-
-    def fill_missingvalues(self,input_df):
-        """
-        vacancy type and gqytpe should be 0 for 
-        occupied housing units
-
-        numprec should be 0 for vacant housing units
-
-        Vacancy should be 0 for Group Quarters
-        gqtype should be 0 for vacant housing units
-
-        consider making all categorical variables have 0 values
-        for observations where category is not applicable
-        vancant housing units race = 0 and ownershp = 0 for example
-        """
-
-        output_df = input_df.copy()
-
-        # Conditions for occupied housing units
-        occupied_hu = (output_df['numprec'] > 0)
-        not_gqtype  = (output_df['gqtype'].isnull())
-        not_vacant  = (output_df['vacancy'].isnull())
-        conditions = occupied_hu & not_gqtype & not_vacant
-        # Fill in missing values
-        output_df.loc[conditions, 'gqtype'] = 0
-        output_df.loc[conditions, 'vacancy'] = 0
-    
-        # Conditions for vacant housing units
-        vacant_hu = (output_df['vacancy'] > 0)
-        not_occupied  = (output_df['numprec'].isnull())
-        conditions = vacant_hu & not_occupied
-        output_df.loc[conditions, 'numprec'] = 0
-        output_df.loc[conditions, 'gqtype'] = 0
-
-        # Conditions for group quarters housing units
-        gq_hu = (output_df['gqtype'] > 0)
-        occupied  = (output_df['numprec'].notnull())
-        conditions = gq_hu & occupied
-        output_df.loc[conditions, 'vacancy'] = 0
-
-
-        return output_df
-
-    def add_hhinc_groups(self,input_df):
-        """
-        Add 5 income groups
-        """
-
-        output_df = input_df.copy()
-
-        hhinc_dict = {1: {'minincome': 0, 'maxincome': 14999},
-        2: {'minincome': 15000, 'maxincome': 24999},
-        3: {'minincome': 25000, 'maxincome': 74999},
-        4: {'minincome': 75000, 'maxincome': 99999},
-        5: {'minincome': 100000, 'maxincome': 10000000}}
-
-        for hhinc in hhinc_dict:
-            randincome_greater_than = (output_df['randincomeB19101'] >= hhinc_dict[hhinc]['minincome'])
-            randincome_less_than    = (output_df['randincomeB19101'] <= hhinc_dict[hhinc]['maxincome'])
-            conditions = randincome_greater_than & randincome_less_than
-            output_df.loc[conditions,'hhinc'] = hhinc
-
-        # Add 0 hhinc - for no income group
-        randincome_missing =  (output_df['randincomeB19101'].isnull())
-        is_gqtype  = (output_df['gqtype'] > 0)
-        is_vacant  = (output_df['vacancy'] > 0)
-        conditions = randincome_missing | is_gqtype | is_vacant
-        output_df.loc[conditions,'hhinc'] = 0
-
-        return output_df
-
-
-    @staticmethod
-    def drop_extra_columns(input_df):
-        """
-        Need to drop columns used to predict variables
-        """
-
-        output_df = input_df.copy()
-
-        drop_if_starts_with = ['prob_','hucount_','preccount_','sumby_','min','max','totalprob_']
-        for substring in drop_if_starts_with:
-            drop_vars = [col for col in output_df if col.startswith(substring)]
-            #print(drop_vars)
-            # Drop columns
-            output_df = output_df.drop(drop_vars, axis=1)
-
-        drop_if_ends_with = ['_counter','_flagset','_flag','_flagsetrm']
-        for substring in drop_if_ends_with:
-            drop_vars = [col for col in output_df if col.endswith(substring)]
-            #print(drop_vars)
-            # Drop columns
-            output_df = output_df.drop(drop_vars, axis=1)
-
-        drop_if_contains = ['byP','byH']
-        for substring in drop_if_contains:
-            drop_vars = [col for col in output_df if substring in col]
-            #print(drop_vars)
-            # Drop columns
-            output_df = output_df.drop(drop_vars, axis=1)
-
-        return output_df
 
     def save_incore_version2(self, input_df):
         """
@@ -548,13 +324,13 @@ class hui_workflow_functions():
         output_df = input_df.copy()
         try:
             print("Add random income.")
-            output_df = self.add_randincome(output_df,self.seed)
+            output_df = add_randincome(output_df,self.seed)
             print("Add poverty.")
-            output_df = self.add_poverty(output_df)
+            output_df = add_poverty(output_df)
             print("Make category 0 for numprec, vacancy and gqtype")  
-            output_df = self.fill_missingvalues(output_df)
+            output_df = fill_missingvalues(output_df)
             print("Drop extra columns.")                                                                  
-            output_df = self.drop_extra_columns(output_df)
+            output_df = drop_extra_columns(output_df)
         except:
             print('Data not ready for final polish.')
 
---
+++ pyncoda/CommunitySourceData/api_census_gov/acg_05b_prec_functions.py
@@ -0,0 +1,221 @@
+# Copyright (c) 2021 Nathanael Rosenheim. All rights reserved.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Mozilla Public License v2.0 which accompanies this distribution,
+# and is available at https://www.mozilla.org/en-US/MPL/2.0/
+
+import numpy as np
+import pandas as pd
+import os # For saving output to path
+import sys
+
+# For printing conda list in log file
+from IPython import get_ipython
+ipython = get_ipython()
+# looks like log overlaps in the output of conda list
+# try pausing for a second
+import time
+
+# Save output as a log file function
+from pyncoda \
+     import ncoda_00c_save_output_log as logfile
+     
+# Load in data structure dictionaries
+from pyncoda.CommunitySourceData.api_census_gov.acg_00a_createAPI_datastructure import *
+from pyncoda.CommunitySourceData.api_census_gov.acg_00a_general_datastructures import *
+from pyncoda.CommunitySourceData.api_census_gov.acg_00f_preci_block2010 import *
+from pyncoda.CommunitySourceData.api_census_gov.acg_00e_incore_huiv2 \
+    import incore_v2_DataStructure
+
+# open, read, and execute python program with reusable commands
+from pyncoda.CommunitySourceData.api_census_gov.acg_01a_BaseInventory import BaseInventory
+from pyncoda.CommunitySourceData.api_census_gov.acg_02a_add_categorical_char \
+     import add_new_char_by_random_merge_2dfs
+from pyncoda.CommunitySourceData.api_census_gov.acg_02c_agefunctions \
+     import *
+
+class prec_workflow_functions():
+    """
+    Function runs full process for generating the person record files
+    Process runs for 1 county.
+    """
+
+    def __init__(self,
+            state_county: str,
+            state_county_name: str,
+            seed: int = 9876,
+            version: str = '3.0.0',
+            version_text: str = 'v3-0-0',
+            basevintage: str = 2010,
+            outputfolder: str ="",
+            outputfolders = {},
+            savefiles: bool = True):
+
+        self.state_county = state_county
+        self.state_county_name = state_county_name
+        self.seed = seed
+        self.version = version
+        self.version_text = version_text
+        self.basevintage = basevintage
+        self.outputfolder = outputfolder
+        self.outputfolders = outputfolders
+        self.savefiles = savefiles
+
+
+    def save_environment_version_details(self):
+        print("\n***************************************")
+        print("    Version control - list of installed packages")
+        print("***************************************\n")       
+
+        try:
+            # print a list of all installed packages and version information
+            ipython.magic("conda list")
+            # Give ipython a second to output results
+            # this step fixes issue with conda list being split by next command
+            time.sleep(1)
+        except:
+            print("Unable to print version information")
+
+
+
+    """
+    Code from 
+    github.com\npr99\Population_Inventory\pyincore_data_addons\SourceData\api_census_gov
+    """
+    
+    def run_prec_workflow(self, savelog=True):
+        """
+        Workflow to produce Person Record Inventory
+        """
+        # Start empty containers to store block level and tract level data
+        tract_df = {}
+        block_df = {}
+
+        # Save output description as text
+        output_filename = f'prec_{self.version_text}_{self.state_county}_{self.basevintage}_rs{self.seed}'
+        self.output_filename = output_filename
+        if savelog == True:
+            log_filepath = self.outputfolders['logfiles']+"/"+output_filename+'.log'
+            # start log file
+            logfile.start(log_filepath)
+            self.save_environment_version_details()
+
+        print("\n***************************************")
+        print("    Obtain and clean core person record characteristics for",self.state_county_name)
+        print("***************************************\n")
+
+        print(self.outputfolders)
+        # Generate Person by Age, Sex, Race, and Hispanic
+        block_df["preci"] = BaseInventory.get_apidata(state_county = self.state_county,
+                                        geo_level = 'block',
+                                        vintage = "2010", 
+                                        mutually_exclusive_varstems_roots_dictionaries =
+                                                            [sexbyage_P12_varstem_roots],
+                                        outputfolders = self.outputfolders,
+                                        outputfile = "CorePRECI")
+
+        block_df["precihispan"] = BaseInventory.graft_on_new_char(base_inventory= block_df['preci'],
+                                        state_county = self.state_county,
+                                        new_char = 'hispan',
+                                        new_char_dictionaries = [sexbyage_P12HAI_varstem_roots,
+                                            hispan_byrace_P5_varstem_roots
+                                            ],
+                                        outputfile = "preci",
+                                        outputfolders = self.outputfolders)
+
+        # Generate sex by age with individual years
+        vintage = '2010'
+        dataset_name = 'dec/sf1' 
+        group = 'PCT12'
+        sexbyage_PCT12 = createAPI_datastructure.obtain_api_metadata(
+                            vintage = vintage,
+                            dataset_name = dataset_name,
+                            group = group,
+                            outputfolder = self.outputfolder)
+
+        tract_df["PCT12"] = BaseInventory.get_apidata(state_county = self.state_county,
+                                        geo_level = 'tract',
+                                        vintage = "2010", 
+                                        mutually_exclusive_varstems_roots_dictionaries =
+                                                            [sexbyage_PCT12],
+                                        outputfolders = self.outputfolders,
+                                        outputfile = group)
+
+        # Add random age to block_df["precihispan"]
+        block_df["precihispan"] = add_randage(
+                                    block_df["precihispan"],
+                                    seed = self.seed,
+                                    varname = 'randageP12')
+        # Add agegroups to block_df["precihispan"]
+        block_df["precihispan"] = add_P12age_groups(
+                                    block_df["precihispan"],
+                                    varname = 'randageP12')
+        # Add random age to tract_df["PCT12"]
+        tract_df["PCT12"] = add_randage(
+                                    tract_df["PCT12"],
+                                    seed = self.seed,
+                                    varname = 'randagePCT12')
+        # Add agegroups to tract_df["PCT12"]
+        tract_df["PCT12"] = add_P12age_groups(
+                                    tract_df["PCT12"],
+                                    varname = 'randagePCT12')
+
+                        
+        print("\n***************************************")
+        print("    Person Block by Age with Tract by Age data.")
+        print("***************************************\n")
+
+        add_age = add_new_char_by_random_merge_2dfs(
+            dfs = {'primary'  : {'data': block_df["precihispan"], 
+                            'primarykey' : 'precid',
+                            'geolevel' : 'Block',
+                            'geovintage' :'2010',
+                            'notes' : 'Block agegroup, sex, race, ethnicity data.'},
+                'secondary' : {'data': tract_df["PCT12"], 
+                            'primarykey' : 'uniqueidPCT12',
+                            'geolevel' : 'Tract',
+                            'geovintage' :'2010',
+                            'notes' : 'Tract single age years, sex data.'}},
+            seed = self.seed,
+            common_group_vars = ['agegroupP12'],
+            new_char = 'randagePCT12',
+            geolevel = "Tract",
+            geovintage = "2010",
+            by_groups = {'All' : {'by_variables' : ['sex']}},
+            fillna_value= -999,
+            state_county = self.state_county,
+            outputfile = "preci_randomage",
+            outputfolder = self.outputfolders['RandomMerge'])
+
+        # Set up round options
+        rounds = {'options': {
+                'option1' : {'notes' : 'By original common group vars and by groups variables.',
+                            'common_group_vars' : 
+                                    add_age.common_group_vars,
+                            'by_groups' :
+                                    add_age.by_groups}
+                                },
+                'geo_levels' : ['Tract']                         
+                }
+
+        prec_age_df = add_age.run_random_merge_2dfs(rounds)
+
+        print("\n***************************************")
+        print("    Try to polish final hui data.")
+        print("***************************************\n")
+
+        prec_df = self.final_prec_polish(prec_age_df['primary'])
+
+        print("\n***************************************")
+        print("    Save cleaned data file.")
+        print("***************************************\n")
+
+        csv_filepath = self.outputfolders['top']+"/"+output_filename+'.csv'
+        savefile = sys.path[0]+"/"+csv_filepath
+        prec_df.to_csv(savefile, index=False)
+        print("File saved:",savefile)
+
+        # Stop log file
+        logfile.stop()
+
+        return prec_df
